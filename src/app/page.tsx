f60945f438d425fa75b471c9ec5b419755262e42
Chirag Jain <dungarwalsaab@gmail.com>
2024-12-29 23:51:50 +0530

    added minor changes to fix build fail issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import useSWR from "swr";
 import Header from "./component/Header";
 import TicketCounter from "./component/TicketCounter";
 import TicketWindow from "./component/TicketWindow";
+import { useEffect } from "react";
 
 // Create a fetcher function for useSWR
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Home() {
   const res = useSWR("/api/verify", fetcher);
-  if (res.error) {
-    localStorage.removeItem("user");
-  }
+  useEffect(() => {
+    if (res.error) {
+      localStorage.removeItem("user");
+    }
+  }, [res]);
+
   return (
     <div className="h-full max-w-screen bg-blue-50">
       <Header />
